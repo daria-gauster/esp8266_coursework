0f900399c6c6e17688d6d4be631218c8284844ee
daria-gauster <74653076+daria-gauster@users.noreply.github.com>
2020-12-04 13:45:00 +0200

    Create index.c

---
+++ index.c
@@ -0,0 +1,26 @@
+#define PIN_LED 10
+#define PIN_PHOTO_SENSOR A0
+#define PIN_RELAY 10
+#define PIN_PHOTO_SENSOR A0
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_RELAY, HIGH);
+}
+
+void loop() {
+  int val = 1023 – analogRead(PIN_PHOTO_RESISTOR);
+  Serial.println(val);
+
+  int ledPower = map(val, 0, 1023, 0, 255); // Преобразуем полученное значение в уровень PWM-сигнала. Чем меньше значение освещенности, тем меньше мощности мы должны подавать на светодиод через ШИМ.
+
+  analogWrite(PIN_LED, ledPower); // Меняем яркость
+  if (val < 300) {
+    // Светло, выключаем реле
+    digitalWrite(PIN_RELAY, HIGH);
+  } else {
+    // Темновато, включаем лампочку
+    digitalWrite(PIN_RELAY,  LOW);
+  }
+}
